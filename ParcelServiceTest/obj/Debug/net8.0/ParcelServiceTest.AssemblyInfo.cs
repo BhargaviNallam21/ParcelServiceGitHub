ff640e08604efda9e8b4335e964ec379c82b8cbd
BhargaviNallam21 <bhargavi.nallam21@gmail.com>
2025-06-13 11:34:48 -0400

    updated prod connectionstring

--- ParcelServiceTest/obj/Debug/net8.0/ParcelServiceTest.AssemblyInfo.cs
+++ ParcelServiceTest/obj/Debug/net8.0/ParcelServiceTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParcelServiceTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3deb7e92f746e5be7bdd153d61781b4cd3d276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab545876ae5b165be67ed341a1f6f10616b2670")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParcelServiceTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParcelServiceTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
